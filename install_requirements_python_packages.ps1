380123484fd918222c11f6358ee22606ed0edc11
Ramesh Babu <133124890+rameshvbabu@users.noreply.github.com>
2024-10-01 16:23:36 +0530

    Create install_requirements_python_packages.ps1

---
+++ install_requirements_python_packages.ps1
@@ -0,0 +1,11 @@
+Invoke-WebRequest https://raw.githubusercontent.com/rameshvbabu/cdw-requirements/refs/heads/main/requirements.txt -Outfile requirements.txt
+write-Host "Latest requirements file downloaded"
+python -m venv .venv
+.\.venv\Scripts\activate
+pip list
+python.exe -m pip install --upgrade pip
+pip list
+write-Host "Installing packages from requirements file"
+pip install -r requirements.txt
+pip list
+write-Host "All packages installed successfully"
